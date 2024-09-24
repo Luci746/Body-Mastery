e3bdcf6baf2ac1748f1cde252d6fcb09854e0923
matheustorrens <matheus.cacemiro@gmail.com>
2024-09-24 16:50:52 -0300

    Salvando remoção

---
+++ setup/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+import usuarios.urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('usuarios/', include(usuarios.urls)),
+
+]
