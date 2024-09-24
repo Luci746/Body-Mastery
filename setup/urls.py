c5a150436bcc1a833bdc305f0e64a6b32cefe93d
matheustorrens <matheus.cacemiro@gmail.com>
2024-09-24 16:52:50 -0300

    Removendo conteúdo da main

--- setup/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-import usuarios.urls
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('usuarios/', include(usuarios.urls)),
-
-]
--- usuarios/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from usuarios import views
-
-urlpatterns = [
-    path('cadastro', views.cadastro, name='cadastro')
-]
